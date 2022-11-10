ca80ffdda7744226353bbf038b3e4892ee8af88a
Upasana <upasana.dhameliya@gmail.com>
2022-11-10 20:04:19 +0530

    feat: movie models and migrations

---
+++ movieslist/movies/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+class Movie(models.Model):
+	title = models.CharField(max_length=255)
+	description = models.CharField(max_length=500, blank=True, null=True)
+	release_date = models.DateField(blank=True,null=True)
+	votes = models.IntegerField(default=0)
+
+class Actor(models.Model):
+	name = models.CharField(max_length=255)
+	dob = models.DateField(null=True,blank=True)
+
+class Cast(models.Model):
+	actor = models.ForeignKey('Actor', on_delete=models.CASCADE)
+	movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
